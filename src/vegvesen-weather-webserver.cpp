931146effc0aea2302f4cb28436bf0f654188e89
Claus Guttesen <kometen@gmail.com>
2016-03-05 18:23:11 +0100

    Raised default connections.

--- src/vegvesen-weather-webserver.cpp
+++ src/vegvesen-weather-webserver.cpp
@@ -25,8 +25,8 @@
 typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 
 int main() {
-    HttpServer server(8080, 4);
-    gnome::Database database {10};
+    HttpServer server(8080, 8);
+    gnome::Database database {50};
 
     // GET site id. Respond with weather statistics.
     server.resource["^/site/([0-9]{1,9})$"]["GET"] = [&database](HttpServer::Response& response, std::shared_ptr<HttpServer::Request> request) {

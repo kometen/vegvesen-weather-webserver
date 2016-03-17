e38b9cc93f5c07218713d4d32a4d3e7b59292faa
Claus Guttesen <kometen@gmail.com>
2016-03-17 20:35:10 +0100

    Added graticule information.

--- src/vegvesen-weather-webserver.cpp
+++ src/vegvesen-weather-webserver.cpp
@@ -83,7 +83,7 @@ int main() {
 
     // Default response.
     server.default_resource["GET"] = [](HttpServer::Response& response, std::shared_ptr<HttpServer::Request> request) {
-        const std::string msg = "<!doctype html><html lang='en'><head><meta charset='utf-8'><title>Weather statistics</title></head><body>Available paths are <a href='/info'>/info</a> and <a href='/site/123'>/site/id</a> where id is numeric</body></html>";
+        const std::string msg = "<!doctype html><html lang='en'><head><meta charset='utf-8'><title>Weather statistics</title></head><body>Available paths<br/><a href='/info'>/info</a><br/><a href='/site/123'>/site/id</a> where id is numeric<br/><a href='/graticule/61.737785/6.4092124'>/graticule/latitude/longitude</a></body></html>";
         const int msgs = msg.size();
         response << "HTTP/1.1 404 Not Found\r\nContent-Length: " << msgs << "\r\n\r\n" << msg;
     };
